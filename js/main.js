bd6a5d8578335e4d7070753cee7bd7f50909e31e
Colin Bos <1337bos@gmail.com>
2016-11-20 00:22:31 -0500

    Cleaned up JS (make that linter happy)
    actually working favicon >.>

--- js/main.js
+++ js/main.js
@@ -1,44 +1,46 @@
+/*jslint browser: true*/
+/*global $, jQuery, alert*/
+
 //jQuery stuff
 $(document).ready(function () {
+    "use strict";
     //Sets the height of the hero image div to the viewport height
     function setHeight() {
-        windowHeight = $(window).innerHeight();
-        windowWidth = $(window).outerWidth();
-        //If it's mobile, set to full height
-        if (windowWidth < 650) {
-            $('#hero').css('min-height', windowHeight)
-        }
+        var windowHeight = $(window).innerHeight(),
+            windowWidth = $(window).outerWidth();
         /*
+            If it's mobile, set to full height
+            
             If you're on your desktop, and you make the viewport smaller, when you size it larger, the proper height needs to be reset.
+            
+            last else if is to support full fold on devices with heigh less than 500px
         */
-        else if (windowHeight > 500 && windowWidth >= 650) {
-            $('#hero').css('min-height', 500)
+        if (windowWidth < 650) {
+            $('#hero').css('min-height', windowHeight);
+        } else if (windowHeight > 500 && windowWidth >= 650) {
+            $('#hero').css('min-height', 500);
+        } else if (windowHeight < 500 && windowWidth >= 650) {
+            $('#hero').css('min-height', windowHeight);
         }
-        /*
-            This preserves the "full fold" use on mobile devices in horizontal mode
-        */
-        else if (windowHeight < 500 && windowWidth >= 650) {
-            $('#hero').css('min-height', windowHeight)
-        }
-    };
+    }
     
     //Sets the top margin for the hero content to center it veritcally
     function setMargin() {
-        windowHeight = $(window).innerHeight();
-        windowWidth = $(window).outerWidth();
-        marginValue = 165;
+        var windowHeight = $(window).innerHeight(),
+            windowWidth = $(window).outerWidth(),
+            marginValue = 165;
         
         if (windowHeight < 500) {
-                marginValue = (windowHeight / 2)-85;
-            }
+            marginValue = (windowHeight / 2) - 85;
+        }
         
         // vertical stack of contents on mobile requires some tweaking
         if (windowWidth < 650) {
-            marginValue = (windowHeight / 2)-195;
+            marginValue = (windowHeight / 2) - 195;
         }
         
         $('#heroContent').css('margin-top', marginValue);
-    };
+    }
     
     //called on page load
     setHeight();
