297afa3d16cd5874106bcfcffe392469cd42aea0
Colin Bos <1337bos@gmail.com>
2016-11-19 23:26:18 -0500

    Tweaked the way it works on desktop, will resubmit.
    grr, this is frustrate.
    also, changed the div size and way the image in the hero is scaled.

--- js/main.js
+++ js/main.js
@@ -3,18 +3,28 @@ $(document).ready(function() {
     //Sets the height of the hero image div to the viewport height
     function setHeight() {
         windowHeight = $(window).innerHeight();
-        $('#hero').css('min-height', windowHeight)
+        windowWidth = $(window).outerWidth();
+        //If it's mobile, set to full height
+        if (windowWidth < 650) {
+            $('#hero').css('min-height', windowHeight)
+        }
+        /*
+            If you're on your desktop, and you make the viewport smaller, when you size it larger, the proper height needs to be reset.
+        */
+        else {
+            $('#hero').css('min-height', 500)
+        }
     };
     
     //Sets the top margin for the hero content to center it veritcally
     function setMargin() {
         windowHeight = $(window).innerHeight();
-        windowWidth = $(window).innerWidth();
-        marginValue = (windowHeight/2)-80;
+        windowWidth = $(window).outerWidth();
+        marginValue = 165;
         
         // vertical stack of contents on mobile requires some tweaking
         if (windowWidth < 650) {
-            marginValue = (windowHeight/2)-130;
+            marginValue = (windowHeight/2)-180;
         }
         
         $('#heroContent').css('margin-top', marginValue)
